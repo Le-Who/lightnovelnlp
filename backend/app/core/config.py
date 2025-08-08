980e0c9fb0fcda06ba5f2afdae2853cd10366751
Le-Who <danoner123@protonmail.com>
2025-08-08 10:39:27 +0300

    modified:   backend/Dockerfile 	modified:   backend/app/core/config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 from typing import List
 
-from pydantic import BaseSettings, Field
+from pydantic_settings import BaseSettings
+from pydantic import Field
 
 
 class Settings(BaseSettings):
