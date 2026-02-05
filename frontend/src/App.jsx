7c1d59973d7be9136f08b38cdbd400142a2e554a
Le-Who <98623572+Le-Who@users.noreply.github.com>
2026-02-05 02:19:34 +0200

    Revert "Palette theme selector 17210910632050558285"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,17 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import DashboardPage from './pages/DashboardPage.jsx'
 import ProjectPage from './pages/ProjectPage.jsx'
-// import TestPage from './pages/TestPage.jsx'
-import { AppLayout } from './components/layout/AppLayout'
 
 export default function App() {
   return (
     <Router>
-      <AppLayout>
+      <div style={{ maxWidth: 960, margin: '0 auto', padding: 24 }}>
+        <h1>Ranobe Translator</h1>
         <Routes>
           <Route path="/" element={<DashboardPage />} />
           <Route path="/projects/:projectId" element={<ProjectPage />} />
-          {/* <Route path="/test" element={<TestPage />} /> */}
         </Routes>
-      </AppLayout>
+      </div>
     </Router>
   )
 }
